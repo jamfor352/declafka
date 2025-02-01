a96eab6ccab6ef99cd6fb266800affe28c4ea4b4
James Forward <james@forwardeder.com>
2025-02-01 09:35:31 +0000

    fix: Move listeners to listeners.rs

--- my_kafka_app/src/listeners/listeners.rs
+++ my_kafka_app/src/listeners/listeners.rs
@@ -1,10 +1,33 @@
 use log::info;
+use my_kafka_lib::{get_configuration, KafkaConfig};
+use my_kafka_lib::OffsetReset::LATEST;
+use my_kafka_macros::kafka_listener;
+use utils::deserializers::{string_deserializer, my_struct_deserializer};
 use crate::models::models::MyStruct;
+use crate::utils;
 
+
+pub fn struct_config() -> KafkaConfig {
+    let mut cfg = get_configuration();
+    cfg.auto_offset_reset = LATEST;
+    cfg.consumer_group = "test-json-consumer-group".to_owned();
+    cfg
+}
+
+#[kafka_listener(
+    topic = "topic-text",
+    config = "get_configuration",
+    deserializer = "string_deserializer"
+)]
 pub fn handle_normal_string(message: String) {
     info!("Received text message: {}", message);
 }
 
+#[kafka_listener(
+    topic = "topic-json",
+    config = "struct_config",
+    deserializer = "my_struct_deserializer"
+)]
 pub fn handle_my_struct(message: MyStruct) {
     info!("Received JSON message with field 1 as: {} and field 2 as: {}", message.field1, message.field2);
 }
--- my_kafka_app/src/main.rs
+++ my_kafka_app/src/main.rs
@@ -1,12 +1,7 @@
 
-use crate::utils::deserializers::{my_struct_deserializer, string_deserializer};
 use crate::routes::routes::app;
 use actix_web::HttpServer;
-use log::info;
-use my_kafka_lib::{get_configuration, KafkaConfig};
-use my_kafka_lib::OffsetReset::LATEST;
-use my_kafka_macros::kafka_listener;
-use crate::models::models::MyStruct;
+use listeners::listeners::{handle_my_struct_listener, handle_normal_string_listener};
 
 mod listeners;
 mod models;
@@ -14,44 +9,14 @@ mod utils;
 mod routes;
 
 
-#[kafka_listener(
-    topic = "topic-text",
-    config = "get_configuration",
-    deserializer = "string_deserializer"
-)]
-fn handle_text_message(msg: String) {
-    info!("Received TEXT: {}", msg);
-}
-
-pub fn json_config() -> KafkaConfig {
-    let mut cfg = get_configuration();
-    cfg.auto_offset_reset = LATEST;
-    cfg.consumer_group = "test-json-consumer-group".to_owned();
-    cfg
-}
-
-#[kafka_listener(
-    topic = "topic-json",
-    config = "get_configuration",
-    deserializer = "my_struct_deserializer"
-)]
-fn handle_json_message(msg: MyStruct) {
-    info!("Received JSON: field1='{}', field2={}", msg.field1, msg.field2);
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // init logs
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    // We can now build them:
-    let text_listener = handle_text_message_listener();
-    let json_listener = handle_json_message_listener();
-
-    // Start them
-    text_listener.start();
-    json_listener.start();
-
+    // Start the Kafka Listeners
+    handle_normal_string_listener().start();
+    handle_my_struct_listener().start();
 
     HttpServer::new(|| {app() })
         .bind("127.0.0.1:8080")?
